ea2b797751bad817535225c0ce861aa7a97ae54a
Yassine El-jdid <eljdidyassine@gmail.com>
2024-09-21 18:31:42 +0100

    updated files

--- predict.py
+++ predict.py
@@ -1,26 +1,24 @@
 from cog import BasePredictor, Input
+from langchain_ollama import ChatOllama
 import subprocess
 import requests
 import json
+import time
 
 MODEL_NAME = "llama2"
 URL = 'http://localhost:11434/api/generate'
 
-
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
+        print("Launch ollama server...")
         subprocess.Popen(["ollama", "serve"])
-        # Start server
-        print("Starting ollama server...")
-        subprocess.Popen(["ollama", "serve"])
-        # Download Model and Run
-        print("Downloading/Running model...")
-        subprocess.check_call(["ollama", "run", MODEL_NAME], close_fds=False)
+        time.sleep(2)
+        print("Start", MODEL_NAME + "...")
+        subprocess.check_output(["ollama", "run", MODEL_NAME])
 
     def predict(self, prompt: str = Input(description="Input text for the model")) -> str:
         """Run a single prediction on the model"""
-        
         data = {
             "model": MODEL_NAME,
             "prompt": prompt
@@ -29,7 +27,7 @@ class Predictor(BasePredictor):
         response = requests.post(URL, data=json.dumps(data), headers=headers)
 
         if response.status_code == 200:
-            return response.json()
-        raise "ERROR!!!!"
+            return str(response.json())
+        raise "ERROR!!!!"        
 
         
